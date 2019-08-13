eb808b1eb436ef5b38403692ecc866f290beddc5
Дмитрий Коннов <konnov@outlook.com>
2019-08-13 14:50:16 +0900

    исправлена пагинация

--- src/components/backgroundForm.vue
+++ src/components/backgroundForm.vue
@@ -12,7 +12,12 @@
             
             <div class="tab-pane active" id="link0" aria-expanded="true">
               <div class="row">
-                images
+                images1
+              </div>
+            </div>
+            <div class="tab-pane" id="link1" aria-expanded="true">
+              <div class="row">
+                images2
               </div>
             </div>
 
@@ -23,13 +28,11 @@
             <center>
             <div width=100% style="display: flex;align-items: center; justify-content: center;">
             <ul class="pagination nav nav-pills nav-pills-primary" role="tablist">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link active" data-toggle="tab" href="#link0" role="tablist" aria-expanded="true">1</a></li>
-              <li class="page-item"><a class="page-link" data-toggle="tab" href="#link1" role="tablist" aria-expanded="false">2</a></li>
-              <li class="page-item"><a class="page-link" data-toggle="tab" href="#link2" role="tablist" aria-expanded="false">3</a></li>
-              <li class="page-item"><a class="page-link" data-toggle="tab" href="#link3" role="tablist" aria-expanded="false">4</a></li>
-              <li class="page-item"><a class="page-link" data-toggle="tab" href="#link4" role="tablist" aria-expanded="false">5</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+              <li class="page-item" :class="{ 'active': currentTab == 0 }" @click="currentTab = 0"><a class="page-link" data-toggle="tab" href="#link0" role="tablist" aria-expanded="true">1</a></li>
+              <li class="page-item" :class="{ 'active': currentTab == 1 }" @click="currentTab = 1"><a class="page-link" data-toggle="tab" href="#link1" role="tablist" aria-expanded="false">2</a></li>
+              <li class="page-item" :class="{ 'active': currentTab == 2 }" @click="currentTab = 2"><a class="page-link" data-toggle="tab" href="#link2" role="tablist" aria-expanded="false">3</a></li>
+              <li class="page-item" :class="{ 'active': currentTab == 3 }" @click="currentTab = 3"><a class="page-link" data-toggle="tab" href="#link3" role="tablist" aria-expanded="false">4</a></li>
+              <li class="page-item" :class="{ 'active': currentTab == 4 }" @click="currentTab = 4"><a class="page-link" data-toggle="tab" href="#link4" role="tablist" aria-expanded="false">5</a></li>
             </ul>
             </div>
             <a href="#" class="btn btn-primary btn-link btn-wd" data-dismiss="modal">Закрыть</a>
@@ -49,31 +52,35 @@ export default {
   name: 'backgroundForm',
   data () {
     return {
-
+      currentTab: 0
     }
   },
   methods:{
-
+  
   },
   created() {
     eventEmitter.$on('showBackgroundForm', () => {
       $("#backgroundForm").modal('show');
+      this.currentTab = 0;
     })
   }
 }
 </script>
 
-<style scoped>
-.pagination > .page-item.active > a, 
-.pagination > .page-item > span, 
-.pagination > .page-item > .page-link:focus, 
-.pagination > .page-item > .page-link:hover, 
-.pagination > .page-item > span:focus, 
-.pagination > .page-item > span:hover {
-	background-color: #9c27b0;
-	border-color: #9c27b0;
-	color: #fff;
-	box-shadow: 0 4px 5px 0 rgba(156,39,176,.14),0 1px 10px 0 rgba(156,39,176,.12),0 2px 4px -1px rgba(156,39,176,.2);
-}
-
+<style lang="sass" scoped>
+.pagination 
+  .page-item.active
+    a, 
+    span, 
+    .page-link:focus, 
+    span:focus
+    	background-color: #9c27b0
+	    border-color: #9c27b0
+	    color: #fff
+	    box-shadow: 0 4px 5px 0 rgba(156,39,176,.14),0 1px 10px 0 rgba(156,39,176,.12),0 2px 4px -1px rgba(156,39,176,.2)
+  .page-item 
+    span:hover, .page-link:hover 
+	    background-color: rgb(243, 241, 241)
+	    border-color: rgb(243, 241, 241)
 </style>
+
