d6e06b8ae3cb78bf59c94c75d935b3736fd743e9
Дмитрий Коннов <konnov@outlook.com>
2019-10-16 16:59:32 +0900

    vuelidate for integration form

--- src/components/integrationForm.vue
+++ src/components/integrationForm.vue
@@ -7,6 +7,7 @@
 				<h2 class="card-title text-center">Интеграция</h2>
           
           <div class="card-body">
+          <form class="form" @submit.prevent="save">
           <div class="row">
           <p class="card-description">
             <center>
@@ -14,26 +15,27 @@
             </center>
           </p> 
 
-           <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text userInfoSocial">
-                  <i class="fab fa-facebook-square"></i>     
-                </span>
-              </div>
-              <input v-model="integrationPixel" type="number" class="form-control" placeholder="Facebook Pixel ...">
+          <div class="input-group" :class="{'has-danger': $v.pixel.$error}">
+            <div class="input-group-prepend">
+              <span class="input-group-text userInfoSocial">
+                <i class="fab fa-facebook-square"></i>     
+              </span>
+            </div>
+            <input v-model="pixel" type="number" @input="$v.pixel.$touch" class="form-control" placeholder="Facebook Pixel ...">
             </div>
             <div style="margin-top:-10px; margin-left:30px">
               <small id="emailHelp" class="form-text text-muted">Укажите код счетчика Facebook Pixel. Создать счетчик можно на странице <a href="https://www.facebook.com/events_manager/" target=new>Facebook Events Manager</a></small>
             </div>
 
-            </div>
+           </div>
         
           <center>
             <br>
-            <button type="submit" class="btn btn-primary btn-round" @click="integrationFormSave">Сохранить</button>
+            <button type="submit" class="btn btn-primary btn-round" :disabled="$v.$invalid">Сохранить</button>
             <a href="#pablo" class="btn btn-primary btn-link btn-wd" data-dismiss="modal">Закрыть</a>
           </center>
         </div>
+        </form>
       </div>
     </div>
   </div>
@@ -44,19 +46,25 @@
 <script>
 import {http} from "./../http"
 import {eventEmitter} from "./../main"
+import {required} from "vuelidate/lib/validators/"
 
 export default {
   name: 'integrationForm',
   data () {
     return {
-      integrationPixel: '',
+      pixel: '',
      }
   },
+  validations: {
+    pixel: {
+      required
+    }
+  },
   methods: {
-    integrationFormSave() {
+    save() {
       http.get('integrationFormSave', {
         params:{
-          integrationPixel: this.integrationPixel
+          pixel: this.pixel
         }
       })
       .then(() => {
@@ -68,7 +76,7 @@ export default {
     eventEmitter.$on('integrationFormShow', () => {
       http.get('integrationFormShow')
       .then(response => {
-        this.integrationPixel = response.data.integrationPixel;
+        this.pixel = response.data.pixel;
         $("#integrationForm").modal('show'); 
       });
     })
